868bacc4db8a3f9cdf9e62b0690448aae5110a83
Shabana MA <mashabana751@gmail.com>
2024-02-24 10:41:19 +0530

    2974

---
+++ .dist/leetcode_2974.dart
@@ -0,0 +1,9 @@
+//2974. Minimum Number Game
+
+class Solution {
+  List<int> numberGame(List<int> nums) {
+    nums.sort();
+    
+    return [for (int i = 0; i < nums.length; i++) nums[i.isEven ? i + 1 : i - 1]];
+  }
+}
